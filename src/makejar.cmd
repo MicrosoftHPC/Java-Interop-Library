a3890400e8fbbf914d39044b55ca653de9c352e2
MicrosoftHPC <yidingz@microsoft.com>
2011-07-07 15:09:14 +0800

    Add make jar shell command file for windows.

---
+++ src/makejar.cmd
@@ -0,0 +1,19 @@
+    @echo off
+    set JAVAC="%JAVA_HOME%\bin\javac.exe"
+    set JAR="%JAVA_HOME%\bin\jar.exe"
+    set CLASSPATH=.;%CXF_HOME%\lib\cxf-manifest.jar
+    set JARFILEClient=Microsoft-HpcSession-3.0.jar
+    set JARFILEHost=Microsoft-HpcServiceHost-3.0.jar
+
+    echo Compiling 
+    del src.list
+    FOR /F %%i IN ('dir /b/s *.java') DO  echo %%i >> src.list
+    %javac% -Djava.endorsed.dirs="%CXF_HOME%\lib\endorsed" @src.list
+   
+    
+    echo Create Jar
+    
+    %jar% cfm %JARFILEClient% manifestclient com\microsoft\schemas com\microsoft\hpc\*.class com\microsoft\hpc\*.java com\microsoft\hpc\brokercontroller com\microsoft\hpc\brokerlauncher com\microsoft\hpc\dataservice com\microsoft\hpc\exceptions com\microsoft\hpc\genericservice com\microsoft\hpc\properties com\microsoft\hpc\scheduler com\microsoft\hpc\session com\microsoft\hpc\sessionlauncher org FaultCodeMessageBundle.properties *.wsdl *.xsd StringTableBundle.properties
+
+    %jar% cfm %JARFILEHost% manifesthost com\microsoft\hpc\servicehost
+    echo Build completed
