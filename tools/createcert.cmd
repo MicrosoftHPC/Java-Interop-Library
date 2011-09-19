87a0d9a80bff5d82622320a1e51d1869f8707e0c
Microsoft HPC <microsofthpc@microsoft.com>
2011-09-19 20:50:57 +0800

    Update createcert.cmd, extend expiration date to 09/01/2017

--- tools/createcert.cmd
+++ tools/createcert.cmd
@@ -4,5 +4,5 @@
 @REM   3) Edit the certificate begin and expiration dates if necessary.
 
 
-makecert.exe -r -pe -n "CN=%COMPUTERNAME%" -b 09/01/2009 -e 09/01/2011 -eku 1.3.6.1.5.5.7.3.1 -ss my -sky exchange -sp "Microsoft RSA SChannel Cryptographic Provider" -sy 12 -sr LocalMachine
+makecert.exe -r -pe -n "CN=%COMPUTERNAME%" -b 09/01/2017 -e 09/01/2011 -eku 1.3.6.1.5.5.7.3.1 -ss my -sky exchange -sp "Microsoft RSA SChannel Cryptographic Provider" -sy 12 -sr LocalMachine
 
