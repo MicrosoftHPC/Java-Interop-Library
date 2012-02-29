629c5d9b088bc49929cde1bdb18c0254a35bd101
Yutong Sun <sunyutong@gmail.com>
2012-02-29 10:49:11 +0800

    Update the tool GetCert.

--- tools/GetCert.java
+++ tools/GetCert.java
@@ -2,93 +2,102 @@
 import java.io.*;
 import java.security.*;
 import java.security.cert.*;
+import java.security.cert.Certificate;
 import javax.net.ssl.*;
 import static java.lang.System.out;
 
 public class GetCert {
+	
+	public static void printCert(Certificate cert)
+	{
+		out.println("Certificate:"+cert.toString());
+		if (cert instanceof X509Certificate) {
+			X509Certificate certX509=(X509Certificate)cert;
+			out.println("Version:"+certX509.getVersion());
+			out.println("Serial:"+certX509.getSerialNumber());
+			out.println("Issuer:"+certX509.getIssuerDN());
+			out.println("ValidBy:"+certX509.getNotBefore());
+			out.println("SigAlg:"+certX509.getSigAlgName());
+		}
+		return;
+	}
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args)
+			throws Exception {
 		String hostname;
-		int port = 443;
-		char[] passphrase;
+		int portnumber = 443;
+		char[] password;
 		if (args.length==0 || args.length>2 )
 		{
-			out.println("Usage: java GetCert <hostname> [passphrase]");
+			out.println("Usage: java GetCert <hostname> [password]");
 			return;
 		}
 		hostname = args[0];
-		String p = (args.length == 1) ? "changeit" : args[1];
-		passphrase = p.toCharArray();
+		String pass = (args.length == 1) ? "changeit" : args[1];
+		password = pass.toCharArray();
 
+		SSLContext context = SSLContext.getInstance("TLS");
+		TrustManager tm =new X509TrustManager (){
+			public void checkClientTrusted(X509Certificate[] chain,
+					String authType) throws CertificateException {
+				return;
+			}
+			public void checkServerTrusted(X509Certificate[] chain,
+					String authType) throws CertificateException {
+				return;
+			}
+			public X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+		};
+		context.init(null, new TrustManager[] {tm}, null);
+		SSLSocketFactory factory = context.getSocketFactory();
+		SSLSocket socket = (SSLSocket)factory.createSocket(hostname, portnumber);
+		socket.setSoTimeout(15000);
+		Certificate[] serverCerts=null;
+		try {
+			socket.startHandshake();
+			serverCerts = socket.getSession().getPeerCertificates(); 
+			socket.close();
+			out.println("Server certificates successfully retrieved.");
+		} catch (SSLException e) {
+			out.println("Obtain server certificates failed.");
+			e.printStackTrace();
+			return;
+		}
+		
+		out.println("Open keystore to check and install the certs...");
+		
 		File file = new File("cacerts_new");
 		if (file.isFile() == false) {
 			File dir = new File(System.getProperty("java.home") + File.separatorChar + "lib" + File.separatorChar + "security");
 			file = new File(dir, "cacerts");
 		}
-
 		InputStream in = new FileInputStream(file);
 		KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
-		ks.load(in, passphrase);
+		ks.load(in, password);
 		in.close();
-
-		SSLContext context = SSLContext.getInstance("TLS");
-		TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-		tmf.init(ks);
-		X509TrustManager defaultTrustManager = (X509TrustManager)tmf.getTrustManagers()[0];
-		SavingTrustManager tm = new SavingTrustManager(defaultTrustManager);
-
-		context.init(null, new TrustManager[] {tm}, null);
-		SSLSocketFactory factory = context.getSocketFactory();
-
-		SSLSocket socket = (SSLSocket)factory.createSocket(hostname, port);
-		socket.setSoTimeout(15000);
-		try {
-			socket.startHandshake();
-			socket.close();
-			out.println("The certificate trusted is already in store");
-			return;
-		} catch (SSLException e) {
-			out.println("The certificate trusted is not in store");
+		
+		for (int k = 0; k < serverCerts.length; k++) {
+			Certificate cert = serverCerts[k];
+			System.out.print("-----BEGIN CERTIFICATE-----\n");
+            printCert(cert);
+            System.out.print("\n-----END CERTIFICATE-----\n");
+            
+			if (ks.getCertificateAlias(cert)==null) {
+				String alias = hostname + "-" + (k + 1);
+				ks.setCertificateEntry(alias, cert);
+				out.println("Added the certificate with alias: "+alias);
+			} else {
+				out.println("This certificate is already in store.");
+			}
 		}
-
-		X509Certificate[] chain = tm.chain;
-
-		for (int k = 0; k < chain.length; k++) {
-			X509Certificate cert = chain[k];
-			String alias = hostname + "-" + (k + 1);
-			ks.setCertificateEntry(alias, cert);
-			out.println("Added certificate with alias: "+alias);
-		}
-
+	
 		OutputStream out = new FileOutputStream("cacerts_new");
-		ks.store(out, passphrase);
+		ks.store(out, password);
 		out.close();
-
-	}
-
-	private static class SavingTrustManager implements X509TrustManager {
-
-		private final X509TrustManager tm;
-		private X509Certificate[] chain;
-
-		SavingTrustManager(X509TrustManager tm) {
-			this.tm = tm;
-		}
-
-		public X509Certificate[] getAcceptedIssuers() {
-			throw new UnsupportedOperationException();
-		}
-
-		public void checkClientTrusted(X509Certificate[] chain, String authType)
-		throws CertificateException {
-			throw new UnsupportedOperationException();
-		}
-
-		public void checkServerTrusted(X509Certificate[] chain, String authType)
-		throws CertificateException {
-			this.chain = chain;
-			tm.checkServerTrusted(chain, authType);
-		}
+		
+		return;
 
 	}
 
